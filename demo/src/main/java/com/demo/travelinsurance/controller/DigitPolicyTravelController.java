c7290fa9ca4c86b0ba600040f52e660af67d8cf7
anaswaraj <nayakswaraj05@gmail.com>
2026-02-19 21:06:39 +0530

    ADDING A NEW CONTROLLER

--- demo/src/main/java/com/demo/travelinsurance/controller/DigitPolicyTravelController.java
+++ demo/src/main/java/com/demo/travelinsurance/controller/DigitPolicyTravelController.java
@@ -24,6 +24,8 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -102,6 +104,67 @@ public class DigitPolicyTravelController {
             String response=gson.toJson(policyResponse);
             policyIssuanceLog.setPolicyResponse(response);
             policyIssuanceLogRepository.save(policyIssuanceLog);
+
+
+
+            return new ResponseEntity<>(response,HttpStatus.OK);
+        } catch (PolicyCreationException pe) {
+            logger.error("Inside PolicyCreationException:: " + pe.getMessage());
+            policyIssuanceLog.setErrorLog(pe.getMessage());
+            policyIssuanceLog.setInternalResponse(gson.toJson(pe.getMessage()));
+            policyIssuanceLog.setResponseTimestamp(LocalDateTime.now());
+            policyIssuanceLogRepository.save(policyIssuanceLog);
+            status.setStatusCode(TravelApiConstant.StatusCode.BAD_REQUEST);
+            status.setStatusMessage(pe.getMessage());
+            return new ResponseEntity<>(status, HttpStatus.BAD_REQUEST);
+        } catch (IllegalArgumentException iae) {
+            logger.error("Inside IllegalArgumentException:: " + iae.getMessage());
+            policyIssuanceLog.setErrorLog(iae.getMessage());
+            policyIssuanceLog.setInternalResponse(gson.toJson(iae.getMessage()));
+            policyIssuanceLog.setResponseTimestamp(LocalDateTime.now());
+            policyIssuanceLogRepository.save(policyIssuanceLog);
+            status.setStatusCode(TravelApiConstant.StatusCode.BAD_REQUEST);
+            status.setStatusMessage(iae.getMessage());
+            return new ResponseEntity<>(status, HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            logger.error("Inside Exception:: " + e.getMessage());
+            policyIssuanceLog.setErrorLog(e.getMessage());
+            policyIssuanceLog.setInternalResponse(gson.toJson(e.getMessage()));
+            policyIssuanceLog.setResponseTimestamp(LocalDateTime.now());
+            policyIssuanceLogRepository.save(policyIssuanceLog);
+            status.setStatusCode(TravelApiConstant.StatusCode.INTERNAL_ERROR);
+            status.setStatusMessage(e.getMessage());
+            return new ResponseEntity<>(status, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
+
+    @PostMapping("/createPolicy12")
+    public ResponseEntity<?> createPolicy12(@RequestBody PolicyRequest policyRequest, HttpServletRequest httpReq) {
+        logger.info("start of createPolicy");
+        logger.info("RequestBody:: " + policyRequest);
+        PolicyIssuanceLog policyIssuanceLog = new PolicyIssuanceLog();
+        PolicyResponse policyResponse = new PolicyResponse();
+        Status status = new Status();
+        try {
+            policyIssuanceLog.setBookingId(policyRequest.getBookingId());
+            policyIssuanceLog.setRequestedTimestamp(LocalDateTime.now());
+            policyIssuanceLog.setPolicyRequest(gson.toJson(policyRequest));
+            policyLogInterface.savePolicyToLog(policyIssuanceLog, policyRequest);
+            status.setStatusCode(200);
+            status.setStatusMessage("Success");
+            policyResponse.setStatus(status);
+            policyResponse.setBookingId(policyRequest.getBookingId());
+            policyResponse.setDate(LocalDateTime.now());
+            policyResponse.setPolicyNumber("D"+System.currentTimeMillis());
+            String response=gson.toJson(policyResponse);
+            policyIssuanceLog.setPolicyResponse(response);
+            policyIssuanceLogRepository.save(policyIssuanceLog);
+
+
+
             return new ResponseEntity<>(response,HttpStatus.OK);
         } catch (PolicyCreationException pe) {
             logger.error("Inside PolicyCreationException:: " + pe.getMessage());
